e4e4eaaafcf55a2966f713f1485e318fdbe32b4d
AƧTЯOƆAT <jinglinzhao@gmail.com>
2022-01-19 20:42:32 -0500

    Update extract_ccf.py

--- extract_ccf.py
+++ extract_ccf.py
@@ -27,7 +27,9 @@ start_time  = datetime.now()
 for single_date in daterange(start_date, end_date):
 
     print(single_date.strftime("%Y-%m-%d"))
-    path = ('./data/' + single_date.strftime('%m') + '/' + single_date.strftime('%d'))
+
+    path_prefix = '/gpfs/group/ebf11/default/pipeline/data/neid_solar/v1.1/outputs/jvz5625/'
+    path = path_prefix + 'extracted_ccf/' + single_date.strftime('%m') + '/' + single_date.strftime('%d') + '/'
     if not os.path.exists(path):
         os.makedirs(path)
         print('The new directory ' + path + ' is created!')
@@ -56,10 +58,10 @@ for single_date in daterange(start_date, end_date):
                         CCF = ccf_per_obs
                     else:
                         CCF = np.vstack((CCF, ccf_per_obs)) 
-                np.savetxt(path + '/' + file_ccf[n][-27:-4] + 'ccf', ccf_per_order)
+                np.savetxt(path + file_ccf[n][-27:-4] + 'ccf', ccf_per_order)
             bar()
 
-    np.savetxt('./data/' + single_date.strftime("%Y-%m-%d") + '.CCF', CCF)
+    np.savetxt(path_prefix + 'ccf_by_day/' + single_date.strftime("%Y-%m-%d") + '.CCF', CCF)
 
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
